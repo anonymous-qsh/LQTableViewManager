f32a9af91792ec18a918fd1a5d6e65b491a19998
Little <anonymous.qsh@gmail.com>
2019-03-30 16:27:52 +0800

    Fix look un shown photo back focus.

--- LQTableViewManager/Cells/LQTableViewImageReadOnlyCell.m
+++ LQTableViewManager/Cells/LQTableViewImageReadOnlyCell.m
@@ -49,10 +49,18 @@
 - (void)cellWillAppear {
   _view.frame = CGRectMake(10, 10, SCREEN_WIDTH - 20, SCREEN_WIDTH - 20);
 
+  // this hock will trigger when cell appear, so I should clear itemArray before add image.
+  [_itemsArray removeAllObjects];
+
   [self addImagesThumbnail];
 
   // show pictures for 3 * 3 (demo) @TODO: add surplus click delegate.
   // @TODO: deal with only has 2 images, or 4 images to shown like 2 * 1 or 2 * 2
+}
+
+- (BOOL)hasMoreImage:(NSInteger)i :(NSInteger)shownImageCount {
+  return self.item.imageShownType != LQImageShownTypeAll && i == shownImageCount - 1
+      && shownImageCount < self.item.imageList.count;
 }
 
 - (void)addImagesThumbnail {
@@ -77,60 +85,66 @@
   _view.frame = CGRectMake(10, 10, SCREEN_WIDTH - 20, height);
   self.item.cellHeight = height;
 
+  // this view used to look not shown photo, back to this.
+  UIView *moreView = [[UIView alloc] init];
+
   // show image grid.
   for (NSInteger i = 0; i < self.item.imageList.count; i++) {
-    UIImageView *imageView = [[UIImageView alloc] init];
-    imageView.userInteractionEnabled = YES;
-    [imageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(click:)]];
-    imageView.tag = i;
-    [imageView sd_setImageWithURL:self.item.imageList[(NSUInteger) i] placeholderImage:nil];
-    imageView.backgroundColor = [UIColor grayColor];
-    CGFloat width = (_view.frame.size.width - 40 + 32) / 3;
-    NSInteger row = i / 3;
-    NSInteger col = i % 3;
-    CGFloat x = 2 + col * (2 + width);
-    CGFloat y = 2 + row * (2 + width);
-    imageView.frame = CGRectMake(x, y, width, width);
-
-    LQPhotoItems *items = [[LQPhotoItems alloc] init];
-    items.url = self.item.imageList[(NSUInteger) i];
-    items.sourceView = imageView;
-    [self.itemsArray addObject:items];
-
-    // add shown image view.
     if (i < shownImageCount) {
+      UIImageView *imageView = [[UIImageView alloc] init];
+      imageView.userInteractionEnabled = YES;
+      [imageView addGestureRecognizer:[[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(click:)]];
+      imageView.tag = i;
+      [imageView sd_setImageWithURL:self.item.imageList[(NSUInteger) i] placeholderImage:nil];
+      imageView.backgroundColor = [UIColor grayColor];
+      CGFloat width = (_view.frame.size.width - 40 + 32) / 3;
+      NSInteger row = i / 3;
+      NSInteger col = i % 3;
+      CGFloat x = 2 + col * (2 + width);
+      CGFloat y = 2 + row * (2 + width);
+      imageView.frame = CGRectMake(x, y, width, width);
+
+      LQPhotoItems *items = [[LQPhotoItems alloc] init];
+      items.url = self.item.imageList[(NSUInteger) i];
+      items.sourceView = imageView;
+      [self.itemsArray addObject:items];
+
       [_view addSubview:imageView];
-    }
 
-    // over flow images. not shown all and has more images to be shown.
-    if (self.item.imageShownType != LQImageShownTypeAll && i == shownImageCount - 1
-        && shownImageCount < self.item.imageList.count) {
-      UIView *view = [[UIView alloc] initWithFrame:CGRectMake(0,
-                                                              0,
-                                                              width,
-                                                              width)];
-      view.backgroundColor = [UIColor colorWithHexString:@"FFFFFF" andAlpha:0.6];
-      [imageView addSubview:view];
-      [self.contentView bringSubviewToFront:view];
+      // over flow images. not shown all and has more images to be shown.
+      if (self.item.imageShownType != LQImageShownTypeAll && i == shownImageCount - 1
+          && shownImageCount < self.item.imageList.count) {
+        moreView = [[UIView alloc] initWithFrame:CGRectMake(0,
+                                                            0,
+                                                            width,
+                                                            width)];
+        moreView.backgroundColor = [UIColor colorWithHexString:@"FFFFFF" andAlpha:0.6];
+        [imageView addSubview:moreView];
+        [self.contentView bringSubviewToFront:moreView];
 
-      UILabel *surplusLabel = [[UILabel alloc] init];
+        UILabel *surplusLabel = [[UILabel alloc] init];
 
-      surplusLabel.text =
-          [NSString stringWithFormat:@"+%@", @(self.item.imageList.count - shownImageCount + 1)];
-      surplusLabel.font = [UIFont systemFontOfSize:25];
-      surplusLabel.textColor = [UIColor whiteColor];
+        surplusLabel.text =
+            [NSString stringWithFormat:@"+%@", @(self.item.imageList.count - shownImageCount + 1)];
+        surplusLabel.font = [UIFont systemFontOfSize:25];
+        surplusLabel.textColor = [UIColor whiteColor];
 
-      [view addSubview:surplusLabel];
+        [moreView addSubview:surplusLabel];
 
-      [surplusLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.center.equalTo(view);
-      }];
+        [surplusLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+          make.center.equalTo(moreView);
+        }];
 
-      [self.contentView bringSubviewToFront:surplusLabel];
+        [self.contentView bringSubviewToFront:surplusLabel];
+      }
+    } else {
+      LQPhotoItems *items = [[LQPhotoItems alloc] init];
+      items.url = self.item.imageList[(NSUInteger) i];
+      items.sourceView = moreView;
+      [self.itemsArray addObject:items];
     }
   }
 }
-
 
 - (void)click:(UITapGestureRecognizer *)tap {
   LQPhotoBrowse *photoBrowse = [[LQPhotoBrowse alloc] init];
